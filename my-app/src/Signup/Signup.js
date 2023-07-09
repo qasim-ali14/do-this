9f5fedfc00e29324dcfd7ce65f7c327d87e3ade8
Qasim ali sajjad <qali64620@gmail.com>
2023-07-10 00:53:45 +0500

    add a sign up setup

---
+++ my-app/src/Signup/Signup.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Signup.css'
+ function Signup() {
+  return (
+   
+    <div className='main'>
+    <div className="signup">
+        <h1>Sign up</h1>
+        <form id="form-start">
+            <label htmlFor="Name">Name</label>
+            <input type="text" placeholder='enter your name' maxLength={12}/>
+            <label htmlFor="f.name">F.name</label>
+            <input type="text" placeholder='enter your f.name'maxLength={12}/>
+            <label htmlFor="age">Age</label>
+            <input type="number" placeholder='enter you age' min={0}/>
+            <label htmlFor="email">E-mail</label>
+            <input type="email" placeholder='xyz@gmail.com' />
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder='*******' maxLength={8} />
+            <label htmlFor="Name">Choose your favorate language:</label>
+            <input type="radio" name="fav-lang" value="html" />
+            <label htmlFor="html">html</label>
+            <input type="radio" name="fav-lang" value="css" />
+            <label htmlFor="css">css</label>
+            <input type="radio" name="fav-lang" value="java" />
+            <label htmlFor="java">java</label>
+            <label htmlFor="Name">select your cars</label>
+            <input type="checknox" name="v1" value="honda"/>
+            <label htmlFor="HONDA">HONDA</label>
+            <input type="checkbox" name="v2" value="mercedes"/>
+            <label htmlFor="MERCEDES">MERCEDES</label>
+            <input type="checkbox" name="v3" value="lamborgini"/>
+            <label htmlFor="LAMBO">LAMBORGINI</label>
+
+        </form>
+    </div>
+    </div>
+  );
+}
+export default Signup;
