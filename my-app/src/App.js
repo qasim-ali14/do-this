321ce7be6395b0bbd5355db530ee970c2a76294b
Qasim ali sajjad <qali64620@gmail.com>
2023-07-09 14:58:50 +0500

    ad  sajjad

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with qasim ali
+          Learn React with qasim ali sajjad
         </a>
       </header>
     </div>
