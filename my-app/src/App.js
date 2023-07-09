348d676111d360e88071e95c22571571500f83ea
Qasim ali sajjad <qali64620@gmail.com>
2023-07-09 15:04:31 +0500

    adding

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with qasim ali sajjad
+          Learn React with qasim ali sajjad adding
         </a>
       </header>
     </div>
