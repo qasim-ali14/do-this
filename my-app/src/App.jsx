82285c1a1d08a9358e5a35da6f69f2c73d22f514
Qasim ali sajjad <qali64620@gmail.com>
2023-07-11 00:06:56 +0500

    props in react

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Checking from "./Checking"
+function App() {
+  return (
+    <>
+   <Checking name="Facebook"/>
+   <Checking name="instagram"/>
+   <Checking name="Google"/>
+  </>
+  );
+}
+
+export default App;
