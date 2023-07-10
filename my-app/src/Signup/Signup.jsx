81d1a638415583806dd0eaac0db45de16b891c1e
Qasim ali sajjad <qali64620@gmail.com>
2023-07-10 10:52:45 +0500

    add some css in jsx file

--- my-app/src/Signup/Signup.jsx
+++ my-app/src/Signup/Signup.jsx
@@ -5,19 +5,19 @@ import './Signup.css'
    
     <div className='main'>
     <div className="signup">
-        <h1>Sign up</h1>
+        <h1 >Sign up</h1>
         <form id="form-start">
-            <label htmlFor="Name">Name</label>
+            <label htmlFor="Name">Name:</label>
             <input type="text" placeholder='enter your name' maxLength={12}/>
-            <label htmlFor="f.name">F.name</label>
+            <label htmlFor="f.name">F.name:</label>
             <input type="text" placeholder='enter your f.name'maxLength={12}/>
-            <label htmlFor="age">Age</label>
+            <label htmlFor="age">Age:</label>
             <input type="number" placeholder='enter you age' min={0}/>
-            <label htmlFor="email">E-mail</label>
+            <label htmlFor="email">E-mail:</label>
             <input type="email" placeholder='xyz@gmail.com' />
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password">Password:</label>
             <input type="password" placeholder='*******' maxLength={8} />
-            <label htmlFor="Name">Choose your favorate language:</label>
+            {/* <label htmlFor="Name">Choose your favorate language:</label>
             <input type="radio" name="fav-lang" value="html" />
             <label htmlFor="html">html</label>
             <input type="radio" name="fav-lang" value="css" />
@@ -25,12 +25,14 @@ import './Signup.css'
             <input type="radio" name="fav-lang" value="java" />
             <label htmlFor="java">java</label>
             <label htmlFor="Name">select your cars</label>
-            <input type="checknox" name="v1" value="honda"/>
             <label htmlFor="HONDA">HONDA</label>
-            <input type="checkbox" name="v2" value="mercedes"/>
+            <input type="checkbox" name="v1" value="honda"/>
             <label htmlFor="MERCEDES">MERCEDES</label>
-            <input type="checkbox" name="v3" value="lamborgini"/>
+            <input type="checkbox" name="v2" value="mercedes"/>
             <label htmlFor="LAMBO">LAMBORGINI</label>
+            <input type="checkbox" name="v3" value="lamborgini"/>
+            */}
+            <button className='btn'>Submit</button>
 
         </form>
     </div>
