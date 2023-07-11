81b9580d69efb6611892d73dbd51199205467fd6
Qasim ali sajjad <qali64620@gmail.com>
2023-07-11 12:23:16 +0500

    add use state hook

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,44 @@
 import React from "react";
 import Checking from "./Checking"
+import Signup from "./Signup/Signup"
+import { useState } from "react";
+
 function App() {
+  const [state,setstate]=useState(0);
+  function increasefun(){
+    setstate(state+1);
+    }
+    function decreasefun(){
+      setstate(state-1);
+    }
+    function removeall(){
+      setstate(state-state);
+    }
+    const styles={
+      btn:{
+    width:'20rem',
+    height:'3rem',
+    border:'2px solid green',
+    margin:'3rem',
+
+      },
+      hell:{
+        marginleft:'33rem',
+        fontsize: '3rem',
+        marginleft: '29rem',
+
+      }
+    }
   return (
     <>
-   <Checking name="Facebook"/>
+    <h1 style={styles.hell}>counting     {state}</h1>
+    <button onClick={increasefun} style={styles.btn}>click me to add</button>
+    <button onClick={decreasefun} style={styles.btn}>click me to remove</button>
+    <button onClick={removeall} style={styles.btn}>click memto remove all</button>
+   {/* <Checking name="Facebook"/>
    <Checking name="instagram"/>
-   <Checking name="Google"/>
+   <Checking name="Google"/> 
+   <Signup/>*/}
   </>
   );
 }
