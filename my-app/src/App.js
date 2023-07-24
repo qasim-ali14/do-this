e92564689f9f3ed241cd99f45cb4831852d1bc24
Qasim ali sajjad <qali64620@gmail.com>
2023-07-24 11:25:03 +0500

    counter make with three hooks(ref,state,effect)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,41 @@
-import React, { useState } from "react";
-import { useRef } from "react";
+import react from 'react'
+import { useState, useEffect, useRef } from 'react';
+
 function App() {
-  //use ref
-  //most used for direct  mamipulate DOM
-  //sometimes we cannot manipulate by state peops so we used this.
-  //we use to manipulate with objevt which ref return.
-  const RefElement=useRef();
-  const [state,setstate]=useState('qasim')
-  console.log(RefElement);
-  //we handle dom with ref by using current which help to change dom
-  function handlein(){
-    RefElement.current.style.color='red';
-  }
-  function reset(){
-    setstate("");
-    RefElement.current.focus();
-  }
+  const [countdown, setCountdown] = useState(10);
+  const intervalRef = useRef(null);
+
+  // Function to start the countdown timer
+  const startCountdown = () => {
+    intervalRef.current = setInterval(() => {
+      setCountdown(prevCountdown => prevCountdown - 1);
+    }, 1000);
+  };
+
+  // Function to stop the countdown timer::::
+  const stopCountdown = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+  };
+
+  // useEffect hook to start the countdown when the component mounts
+  useEffect(() => {
+    startCountdown();
+
+    // Clean up the interval when the component unmounts
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, []); // empty dependency array ensures the effect runs only once on component mount
+
   return (
-    <>
-    <input  ref={RefElement} value={state} onChange={(e)=>setstate(e.target.value)}/>
-    <button onClick={reset}>reset</button>
-    <button onClick={handlein}> handle input</button>
-    </>
+    <div>
+      <h2>Countdown: {countdown}</h2>
+      <button onClick={startCountdown}>Start Countdown</button>
+      <button onClick={stopCountdown}>Stop Countdown</button>
+    </div>
   );
 }
 
