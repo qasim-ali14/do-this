ca9525d0afd73a2dbc8f95bedef85fef29f4aa57
Qasim ali sajjad <qali64620@gmail.com>
2023-07-24 12:24:48 +0500

    props drilling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,21 @@
 import react from 'react'
-import { useState, useEffect, useRef } from 'react';
+import ChildA from './ChildA';
 
 function App() {
-  const [countdown, setCountdown] = useState(10);
-  const intervalRef = useRef(null);
-
-  // Function to start the countdown timer
-  const startCountdown = () => {
-    intervalRef.current = setInterval(() => {
-      setCountdown(prevCountdown => prevCountdown - 1);
-    }, 1000);
-  };
-
-  // Function to stop the countdown timer::::
-  const stopCountdown = () => {
-    clearInterval(intervalRef.current);
-    intervalRef.current = null;
-  };
-
-  // useEffect hook to start the countdown when the component mounts
-  useEffect(() => {
-    startCountdown();
-
-    // Clean up the interval when the component unmounts
-    return () => {
-      if (intervalRef.current) {
-        clearInterval(intervalRef.current);
-      }
-    };
-  }, []); // empty dependency array ensures the effect runs only once on component mount
-
+  //prop drilling
+  //when we want to send data from parent to multiple components ot its third child 
+  //like its nested component, then send data from parent to a to b, to c, to d.
+  //its ok in small application where two to three nested loop made but in 10 to 12 nested loop 
+  //this can create complexity so we can avoid that
+  
+  const name='qasim ali sajjad'
   return (
-    <div>
-      <h2>Countdown: {countdown}</h2>
-      <button onClick={startCountdown}>Start Countdown</button>
-      <button onClick={stopCountdown}>Stop Countdown</button>
-    </div>
-  );
+     <>
+     <ChildA name={name}/>
+     
+     </>
+  )
+  ;
 }
 
 export default App;
