6ab4e07a72da21359d4ace7e525a8a16eda4f80f
Qasim ali sajjad <qali64620@gmail.com>
2023-10-18 13:16:27 +0500

    use memo hook

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import ChildA from './ChildA';
-import react, { createContext } from 'react'
-
-//how to avoid props drilling 
-//we use context api
-//three steps 1.create context 2.provider 3.consumer
-////how to maintain better  context api we usew use context hook:
-
-const data=createContext();
-const gend=createContext();
-function App() {
-
-  const name='Qasim ali '
-  const gender='male'
+import React, { useState,useMemo } from 'react'
+ function App() {
+  const [add,setadd]=useState(0);
+  const [subtract,setsubtract]=useState(110);
+  const hello=useMemo(
+    function  multiply(){//use memo hook use to increase performance like stop
+      //repeating data on other components.
+      console.log("**************");
+      return add*10;//by using memo hook we cccan easily avoid the extra things which previosly
+      // printed by rendering other component also.
+  },[add])
   return (
-     <>
-     <data.Provider value={name}> 
-      <gend.Provider value={gender}>
-     <ChildA/>
-     </gend.Provider>
-     </data.Provider>
-     </>
-  )
-  ;
-}
+    <div>
+      <h1> use memo hook</h1>
+      {hello}
+      <button onClick={()=>{
+        setadd(add+1);
+      }}>addition</button>
+      <span>{add}</span>  
 
+
+      <button  onClick={()=>{
+        setsubtract(subtract-1);
+      }}>minus</button>
+      <span>{subtract}</span>
+    </div>
+  )
+}
 export default App;
-export {data,gend}
--- my-app/src/ChildC.jsx
+++ my-app/src/ChildC.jsx
@@ -2,14 +2,33 @@ import React, { useContext } from 'react';
 import { data,gend } from './App';
 
 function ChildC() {
-  const name=useContext(data);//actua;;u use context the problem like call back hell.
-  const gender=useContext(gend);//so we can  catch props here very easily and maintain this,
+   const name=useContext(data);//actually  context api  the problem like call back hell.
+   const gender=useContext(gend);//so we can  catch props here very easily and maintain this,
   return (
+<>
+<h1> my name is {name}. i am {gender}</h1>
+</>
+    )
+  }
+    // <>
+    // <d>
+    //   {
+    //      (name)=>{
+    //       return(
+    //         <gend.Consumer>
+    //   {
+    //       (gender)=>{
+    //       return(
+    //         <h1>
+    //         my name is {name}. and i am {gender}
+    //       </h1>
+    //       )
+    //     }
+    //   }
+    //  </gend.Consumer>
+    //       )
 
-    <>
-     <h1>hi i am {name}.i am {gender}</h1> 
-    </>
-  );
-}
+     
+ 
 
 export default ChildC;
