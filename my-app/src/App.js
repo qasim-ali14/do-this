8d60d5fe450b2d3e4887f4a319b9bae1733db738
Qasim ali sajjad <qali64620@gmail.com>
2023-10-23 10:11:08 +0500

    uncontrolled component with  use ref

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,23 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 
 //controlled form mean we can controll the submission after the user click on submit button
-//uncontrolled mean that we did not control but dom controlled it  we use default behavior which js giving us.
+//uncontrolled mean that we did not control but dom controlled it  we use default behavior
+// which js giving us. like  dom controll itself.
 function App() {
-  const [name,setname]=useState("");
-  const [lname,setlname]=useState("");
-  const [passs,setpass]=useState("");//s
-   function handle(e){
-    if(e.target.name == "firstname"){ 
-      const capp=(e.target.value).toUpperCase();
-      setname(capp);
-    }
-    else if(e.target.name == "lastname"){   
-      const lower=(e.target.value).toLowerCase();
-      setlname(lower);
-    }
-    else
-    setpass(e.target.value);
+  const inputelement=useRef(0);
+  function handle(){
+    inputelement.current.focus();//with use ref we can easily manipulate dom 
+    //and add things.
   }
   return (
-    <div>
+      <div>
       <label htmlFor="fname">fname</label>
-      <input type='text' name="firstname" value={name} onChange={handle} /><br></br>
-     <label htmlFor="lname">lname</label>
+      <input type='text'  ref={inputelement} /><br></br>
+      <button onClick={handle}>SUBMIT</button>
+      {/* <label htmlFor="lname">lname</label>
       <input type='text' name="lastname" value={lname} onChange={handle}/><br></br>
       <label htmlFor="pass">password</label>
-      <input type='password' name="password" value={passs} onChange={handle}/><br></br>
+      <input type='password' name="password" value={passs} onChange={handle}/><br></br> */}
     </div>
   )
 }
