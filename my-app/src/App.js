dfd73715a8744ee3f57b6e53321ab3e5d95336a2
Qasim ali sajjad <qali64620@gmail.com>
2023-10-27 12:07:52 +0500

    project : make a form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import Child from './Child';
+import Signup from './Signup/Signup';
+
  function App() {
-  function data(data){//we do lifting state when we want that data which user enter
-    // in input in child component then we pass from child to parent...
-console.log(data);
-  }
   return (
     <div>
-    <Child data={data}/>  {/* //here we have a function that accepts data and show that */}
+     
+      <Signup/>
     </div>
   )
 }
+
 export default App;
--- my-app/src/Signup/Signup.jsx
+++ my-app/src/Signup/Signup.jsx
@@ -1,31 +1,46 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import './Signup.css'
 // import '../Checking'
-// import { useState } from 'react';
 
  function Signup() {
-//    const [state,setstate]=useState(0);
-//    function add(){
-//     setstate(state+1);
+  const input={ name:"" , fname: "" }
+   const [data,setdata]=useState(input);
+   const [flag,setflag]=useState(false); 
+  function handle(e){
+     setdata({...data,[e.target.name]:e.target.value});
+     console.log(data);
+  }
+  useEffect(()=>{
+    console.log('registered');
+  },[flag]);
+  const handlesubmit=(e)=>{
+    e.preventDefault();
+    if(!data.name || !data.fname){
+      alert('please complete all input');
+    }
+    else{
+      setflag(true);
+    }
 
-//    }
+  }
+
     return(
+      <>
+      <pre>{(flag)?<h2>congratualtion {data.name} you have sucessfully signup</h2>:""}</pre>
      <div className='main'>
-        
-  <div className="signup">
-       
+    <div className="signup">
       <h1 >Sign up</h1>
-      <form id="form-start">
+       <form id="form-start" onSubmit={handlesubmit}>
           <label htmlFor="Name">Name:</label>
-          <input type="text" placeholder='enter your name' maxLength={12}/>
+          <input type="text" placeholder='enter your name' name='name' value={data.name}  onChange={handle} maxLength={12}/>
           <label htmlFor="f.name">L.name:</label>
-          <input type="text" placeholder='enter your L.name'maxLength={12}/>
-          <label htmlFor="age">Age:</label>
+          <input type="text" placeholder='enter your L.name'name='fname' value={data.fname}  onChange={handle} maxLength={12}/>
+          {/* <label htmlFor="age">Age:</label>
           <input type="number" placeholder='enter you age' min={0}/>
           <label htmlFor="email">E-mail:</label>
           <input type="email" placeholder='xyz@gmail.com' />
           <label htmlFor="password">Password:</label>
-          <input type="password" placeholder='*******' maxLength={8} />
+          <input type="password" placeholder='*******' maxLength={8} /> */}
         
           <button   className='btn'>Submit</button>
          
@@ -34,6 +49,7 @@ import './Signup.css'
       </form>
   </div>
   </div>
+  </>
   );
 }
 export default Signup;
