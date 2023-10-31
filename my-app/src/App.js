abc6dce8bd73c6c86250e926e175bbf3b845a952
Qasim ali sajjad <qali64620@gmail.com>
2023-10-31 12:30:25 +0500

    keys in map function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
  function App() {
-  const app=['qasim','ali','sajjad'];//basiacallt map provide us the extension by the in
+  const app=['qasim','ali','sajjad','qasimcd'];//basiacallt map provide us the extension by the in
   // which when we render value ut render in a seprate function after that this is all store in 
   // a new array.
-  const recive=app.map((value)=>
-  <h1>{value}</h1>
+  const recive=app.map((value,index)=>
+  <h1 key={index}>{value} and our index is {index}</h1>//Keys are used by React to keep track of individual elements within a list. 
+  //They help React understand which elements have been added, removed, or modified.i mean 
+  //by this we can make code efficeint and time saving as it is in react javascript.
  );
   return (
     <div>
