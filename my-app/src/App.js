cd800b64154c181345e71bea8a451fffa0e41729
Qasim ali sajjad <qali64620@gmail.com>
2023-10-31 15:30:23 +0500

    with array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,27 @@
 import React, { useState } from 'react'
-
+import "./App.css"
  function App() {
-     const data={name:"qasim",lname:"ali"}
-  const [value,setvalues]=useState(data)
- function update (){
-  setvalues({...value,name:"ali"})//we are facing the problem the one is update
-  // but other is not showing
-  //so we can use spread operator by this is we actually can create the copy 
-  //of and when we change any thing it can over write there.
- }
+  const [value,setvalue]=useState([]);
+  function add(){
+    setvalue([...value,{
+      id:value.length,
+      item:'qasim ali'
+    }])
+  }
+  
   return (
-    <div>
-      <h1>my first name is {value.name}.and last name is {value.lname}.</h1>
-      <button onClick={update}>update</button>
+
+    <div className='App'>
+     <ol>
+      {
+        value.map((value)=>
+        
+        <li key={value.id}>{value.item}</li>
+        )
+      }
+     </ol>
+     <button onClick={add}>add item</button>
     </div>
   )
 }
-
 export default App;
