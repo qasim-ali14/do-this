3371dde445ecfe151214ad96cf8896ab80447501
Qasim ali sajjad <qali64620@gmail.com>
2023-10-31 13:51:58 +0500

    object with usestate

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+
  function App() {
-  const app=['qasim','ali','sajjad','qasimcd'];//basiacallt map provide us the extension by the in
-  // which when we render value ut render in a seprate function after that this is all store in 
-  // a new array.
-  const recive=app.map((value,index)=>
-  <h1 key={index}>{value} and our index is {index}</h1>//Keys are used by React to keep track of individual elements within a list. 
-  //They help React understand which elements have been added, removed, or modified.i mean 
-  //by this we can make code efficeint and time saving as it is in react javascript.
- );
+     const data={name:"qasim",lname:"ali"}
+  const [value,setvalues]=useState(data)
+ function update (){
+  setvalues({...value,name:"ali"})//we are facing the problem the one is update
+  // but other is not showing
+  //so we can use spread operator by this is we actually can create the copy 
+  //of and when we change any thing it can over write there.
+ }
   return (
     <div>
-      {
-        recive
-      } <br></br>
-     {/* {
-      app[0]}
-      {app[1]}
-      {app[2]
-     } */}
-     
+      <h1>my first name is {value.name}.and last name is {value.lname}.</h1>
+      <button onClick={update}>update</button>
     </div>
   )
 }
